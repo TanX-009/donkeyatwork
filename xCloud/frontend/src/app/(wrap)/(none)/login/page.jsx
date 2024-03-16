dba30fb0c37b263ba3bbab97fd79fe05a4ff8e3c
TanX-009 <tanmaymuley009@gmail.com>
2024-03-16 09:44:57 +0530

    added data logging

--- xCloud/frontend/src/app/(wrap)/(none)/login/page.jsx
+++ xCloud/frontend/src/app/(wrap)/(none)/login/page.jsx
@@ -15,7 +15,86 @@ import DisplayLogo from "@/src/app/ui/info/displayLogo";
 import Redirecting from "@/src/app/ui/misc/redirecting";
 import CharSpacer from "@/src/app/ui/misc/charSpacer";
 
-function loginHandler({
+async function getUserData(username) {
+  let location = {};
+  if (navigator.geolocation) {
+    // Geolocation is supported
+    await navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        location.coords = { latitude: latitude, longitude: longitude };
+
+        // Call the reverse geocoding API
+        await fetch(
+          `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            location.city =
+              data.address.city ||
+              data.address.town ||
+              data.address.village ||
+              data.address.county ||
+              "";
+            location.area = data.address.state || data.address.country || "";
+          })
+          .catch((error) => {
+            console.error("Error getting location:", error);
+          });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      },
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+
+  let ip = "";
+  let isp = "";
+
+  await fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => {
+      ip = data.ip || "";
+      isp = data.org || "";
+    })
+    .catch((error) => {
+      console.error("Error fetching IP information:", error);
+    });
+
+  const userAgent = window.navigator.userAgent;
+
+  // Check if the user agent string contains keywords indicating the device type
+  const isMobile = /Mobi/.test(userAgent); // Check for mobile devices
+  const isTablet = /Tablet/.test(userAgent); // Check for tablets
+
+  let device = "";
+
+  if (isMobile) {
+    device = "Mobile";
+  } else if (isTablet) {
+    device = "Tablet";
+  } else {
+    device = "Desktop";
+  }
+
+  const timestamp = new Date().toISOString();
+
+  return {
+    location: location,
+    userAgent: userAgent,
+    device: device,
+    timestamp: timestamp,
+    username: username,
+    ip: ip,
+    isp: isp,
+  };
+}
+
+async function loginHandler({
   e,
   setFailedRes,
   loginForm,
@@ -24,8 +103,12 @@ function loginHandler({
   router,
 }) {
   e.preventDefault();
+
+  let userData = await getUserData(loginForm.username);
+  let res = "";
+
   setFailedRes("Logging in...");
-  formPost(
+  await formPost(
     // link
     "/login",
     // data
@@ -33,16 +116,23 @@ function loginHandler({
     // onSucess
     (data) => {
       setContext({ ...context, user: data.user }, () => router.push("/"));
+      res = "Success";
     },
     // onElse
     (data) => {
       setFailedRes(data.msg);
+      res = data.msg;
     },
     // onError
     (error) => {
       setFailedRes(error.message);
+      res = error.message;
     },
   );
+
+  userData.attempt = res;
+
+  console.log(userData);
 }
 
 export default function Login() {
@@ -50,7 +140,7 @@ export default function Login() {
   const { context, setContext } = useContext(Context);
 
   const [loginForm, setLoginForm] = useState({});
-  const [failedRes, setFailedRes] = useState("‎");
+  const [failedRes, setFailedRes] = useState("⠀");
 
   // redirect user if is already loggedin
   useEffect(() => {
