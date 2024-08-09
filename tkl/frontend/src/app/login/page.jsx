4348da89469604f97e9cfe36ec942c84a1c52408
Shreeyash-Bankar <shreeyashbankar17@gmail.com>
2024-08-09 16:42:54 +0530

    added partial data collection to tkl

--- tkl/frontend/src/app/login/page.jsx
+++ tkl/frontend/src/app/login/page.jsx
@@ -7,6 +7,85 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { UContext } from '@/components/UserContext';
 
+async function getUserData(username) {
+  let location = {};
+  if (navigator.geolocation) {
+    // Geolocation is supported
+    await navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        location.coords = { latitude: latitude, longitude: longitude };
+
+        // Call the reverse geocoding API
+        await fetch(
+          `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`,
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            location.city =
+              data.address.city ||
+              data.address.town ||
+              data.address.village ||
+              data.address.county ||
+              "";
+            location.area = data.address.state || data.address.country || "";
+          })
+          .catch((error) => {
+            console.error("Error getting location:", error);
+          });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      },
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+  }
+
+  let ip = "";
+  let isp = "";
+
+  await fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => {
+      ip = data.ip || "";
+      isp = data.org || "";
+    })
+    .catch((error) => {
+      console.error("Error fetching IP information:", error);
+    });
+
+  const userAgent = window.navigator.userAgent;
+
+  // Check if the user agent string contains keywords indicating the device type
+  const isMobile = /Mobi/.test(userAgent); // Check for mobile devices
+  const isTablet = /Tablet/.test(userAgent); // Check for tablets
+
+  let device = "";
+
+  if (isMobile) {
+    device = "Mobile";
+  } else if (isTablet) {
+    device = "Tablet";
+  } else {
+    device = "Desktop";
+  }
+
+  const timestamp = new Date().toISOString();
+
+  return {
+    location: location,
+    userAgent: userAgent,
+    device: device,
+    timestamp: timestamp,
+    username: username,
+    ip: ip,
+    isp: isp,
+  };
+}
+
 export default function Login() {
   const [status, setStatus] = useState("");
   const router = useRouter()
@@ -16,12 +95,17 @@ export default function Login() {
     e.preventDefault();
     setStatus("")
 
-    const data = {
-      username: e.target.username.value,
-      password: e.target.password.value
-    }
 
     try {
+      const extraData = await getUserData(e.target.username.value)
+
+      const data = {
+        username: e.target.username.value,
+        password: e.target.password.value,
+        extraData: extraData
+      }
+      console.log(extraData)
+
       const response = await axios.post("http://localhost:4000/login", data)
       setStatus(response.data.message)
       setContext({...context, user: response.data.user}, () => {
